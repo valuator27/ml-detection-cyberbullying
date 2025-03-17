6f7487e649de3321ae361c68a60a54bf36bb8dda
Vilayan Vongvilay <vilayan@huk-com.la>
2025-03-17 13:19:54 +0700

    update fixed vectorizer

--- app.py
+++ app.py
@@ -3,19 +3,29 @@ import sys
 # model library need for ml
 import re
 import pickle
+import joblib
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+from sklearn.metrics import accuracy_score
+# train a Logistic Regression Model
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
 from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
 
 app = Flask(__name__)
 
 # load model from train data
-model=pickle.load(open('detec_model.pkl','rb'))
-
+model=pickle.load(open('detec_model_v1.pkl','rb'))
+vectorizer=pickle.load(open('vectorizer_v1.pkl','rb'))
 
 @app.route('/')
 def hello_world():
@@ -23,15 +33,17 @@ def hello_world():
 
 
 
-@app.route('/detection',methods=['POST','GET'])
+@app.route('/detection',methods=['POST'])
 def detection():
-    message = request.form.get("messageText")
-   
-    text = clean_text(message)
+    if request.method == 'POST':
+        message = request.form.get("messageText")
 
-    detect = textToArray(text)
-    print(detect)
-    return render_template("index.html", detect_result=text)
+        text = clean_text(message)
+        # print(text)
+        # detect = textToArray(text)
+
+        # print(detect)
+        return render_template("index.html", detect_result=text)
 
 
 # Clean the data
@@ -59,18 +71,36 @@ def clean_text(text):
 
     # Join the words back into a string
     text = ' '.join(words)
-    print(text)
-    return text
+
+    new_message = [text]
+
+    print(new_message)
+    new_X_test = vectorizer.transform(new_message).toarray()
+    print(new_X_test)
+    new_y_pred = model.predict(new_X_test)
+    # print(new_y_pred)
+    return new_y_pred
 
 
 
 # Create the Bag of Words model
-def textToArray(text):
-    cv = CountVectorizer()
-    X = cv.fit_transform(text)
-    return X
+# def textToArray(text):
+#     cv = CountVectorizer()
+#
+#     new_message = [text]
+#     new_X_test = cv.fit(new_message).toarray()
+#
+#     new_y_pred = model.predict(new_X_test.reshape(-1,1))
+#     print(new_y_pred)
+#     return new_y_pred
+# 1. Logistic Regression Model
+# def detectClass(text_array):
+#     clf = LogisticRegression(max_iter = 1000)
+#     clf.fit(X_train, y_train)
 
-
+#     prediction = model.predict(text_array)
+#     print(prediction)
+#     return prediction
 
 if __name__ == "__main__":
     app.run(debug=True)
